2b500243b912940c3d0c76fe8588f38b288ba2eb
Rijo George <rijogeorge@rijos-mbp-2.lan>
2022-03-01 07:46:08 -0800

    uuid added to each box

--- src/BoxList.js
+++ src/BoxList.js
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import Box from "./Box";
 import NewBoxForm from "./NewBoxForm";
+import { v4 as uuid } from 'uuid';
 
 const BoxList = () => {
     const getInitialBoxState = () => {
-        const boxes = [{background: 'blue', width: 100, height: 100}]
+        const boxes = [{id: uuid(), background: 'blue', width: 100, height: 100}]
         return boxes;
     }
     const [boxes, setBoxes] = useState(getInitialBoxState)
-    const addBox = (boxObj) => setBoxes([...boxes, boxObj])
+    const addBox = (boxObj) => {
+        boxObj = {...boxObj, id:uuid()}
+        setBoxes([...boxes, boxObj])
+    }
     const boxArray = boxes.map(e => <Box backgroundColor={e.background} width={e.width} height= {e.height} />)
     return (
         <div>
